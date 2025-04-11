1b6dbf392cd39629ffedee43b622425421be8275
Gloria Fulguera <gloriafulguera@gmail.com>
2025-04-10 22:38:25 -0300

    Refactorizacion controller por recarga de tablas. Agrego actions por gestion de productos

--- proj_Bestock/Controllers/TiendaController.cs
+++ proj_Bestock/Controllers/TiendaController.cs
@@ -7,32 +7,55 @@ namespace proj_Bestock.Controllers
     public class TiendaController : Controller
     {
         private readonly TiendaService _tiendaService;
+        //AdmTiendaViewModel _model;
         public TiendaController(TiendaService tiendaService)
         {
             _tiendaService = tiendaService;
         }
         public IActionResult Index()
         {
-            var categorias = _tiendaService.ObtenerCategorias();
-            return View("AdmTienda",categorias);
+            var _model = new AdmTiendaViewModel
+            {
+                Categorias = _tiendaService.ObtenerCategorias(),
+                Productos = _tiendaService.ObtenerProductos()
+            };
+            return View("AdmTienda", _model);
         }
         [HttpPost]
         public IActionResult AgregarCategoria(Categoria model)
         {
             _tiendaService.AgregarCategoria(model);
-            var categorias = _tiendaService.ObtenerCategorias();
-            return View("AdmTienda", categorias);
+            //var categorias = _tiendaService.ObtenerCategorias();
+            return Index();
         }
         public IActionResult EditarCategoria(Categoria nuevaCat)
         {
             _tiendaService.EditarCategoria(nuevaCat);
-            var categorias = _tiendaService.ObtenerCategorias();
-            return View("AdmTienda", categorias);
+            //var categorias = _tiendaService.ObtenerCategorias();
+            return Index();
         }
         public IActionResult Eliminar(int id) {
             _tiendaService.EliminarCategoria(id);
-            var categorias = _tiendaService.ObtenerCategorias();
-            return View("AdmTienda", categorias);
+            //var categorias = _tiendaService.ObtenerCategorias();
+            return Index();
+        }
+
+        public IActionResult CargarProductos()
+        {
+            //var productos=_tiendaService.ObtenerProductos();
+            return Index();
+        }
+
+        public IActionResult ProductoNuevo()
+        {
+            ViewBag.Categorias=_tiendaService.ObtenerCategorias();
+            return View("ProductoNuevo");
+        }
+        [HttpPost]
+        public IActionResult AgregarProducto(Producto model) {
+            //var categorias = _tiendaService.ObtenerCategorias();
+            bool resultado=_tiendaService.AgregarProducto(model);
+            return Index();
         }
     }
 }
